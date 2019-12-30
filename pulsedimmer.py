33f051002bf0c86634e00c4f630fcd6e842691a6
Charles LaBerge <claberge@visi.com>
2019-12-30 14:54:17 -0800

    new file

---
+++ pulsedimmer.py
@@ -0,0 +1,35 @@
+from argparse import ArgumentParser
+from gpiozero import LED 
+from time import sleep
+
+parser = ArgumentParser()
+parser.add_argument('--brightness', action='store', type=float, default=1.0)
+parser.add_argument('--pulse', action='store', type=float, default=0)
+args = parser.parse_args()
+
+pulse = args.pulse
+if pulse < 0:
+    raise ValueError("Pulse length can't be negative")
+
+brightness = args.brightness
+if brightness < 0 or brightness > 1:
+    raise ValueError("Brightness needs to be between 0 and 1 inclusive")
+
+led = LED(18)
+
+on = brightness
+off = 1 - brightness
+cycle = 0.01
+
+while True:
+
+    while float.tick < pulse:
+
+        led.on()
+        sleep(cycle * on)
+        led.off()
+        sleep(cycle * off)
+        tick = tick + cycle
+
+    led.off()
+    sleep(pulse)
