5b6313e38905aca74cd6622454743448ec16a019
Tim LaBerge <tim@timlaberge.com>
2019-12-30 14:09:10 -0800

    Add dimmer

--- dimmer.py
+++ dimmer.py
@@ -6,17 +6,19 @@ parser = ArgumentParser()
 parser.add_argument('--brightness', action='store', type=float, default=1.0)
 args = parser.parse_args()
 
+
+brightness = args.brightness
+if brightness < 0 or brightness > 1:
+    raise ValueError("Brightness needs to be between 0 and 1 inclusive")
+
 led = LED(18)
-led.on()
 
-if round(args.brightness, 2) == 1:
+on = brightness
+off = 1 - brightness
+cycle = 0.01
+
+while True:
     led.on()
-elif round(args.brightness, 2) == 0:
+    sleep(cycle * on)
     led.off()
-else:
-    while True:
-        led.on()
-        sleep(.001 * args.brightness)
-        led.off()
-        sleep(.001 / args.brightness)  
-
+    sleep(cycle * off)
