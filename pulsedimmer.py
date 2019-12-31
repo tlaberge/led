42144de4ce70f657d53e5b60f037ee6d21b7052c
Charles LaBerge <claberge@visi.com>
2019-12-30 16:41:03 -0800

    cycle function and no pulse option

--- pulsedimmer.py
+++ pulsedimmer.py
@@ -7,9 +7,11 @@ parser.add_argument('--brightness', action='store', type=float, default=1.0)
 parser.add_argument('--pulse', action='store', type=float, default=0)
 args = parser.parse_args()
 
-pulse = args.pulse
-if pulse <= 0:
+pulse = args.pulse / 2
+if pulse < 0:
     raise ValueError("Pulse length can't be negative")
+elif 0 < pulse < 0.05:
+    raise ValueError("Pulse length can't be less than 0.1 seconds; set to 0 for no pulse")
 
 brightness = args.brightness
 if brightness < 0 or brightness > 1:
@@ -21,17 +23,31 @@ on = brightness
 off = 1 - brightness
 cycle = 0.01
 
-while True:
 
-    pulse_tick = 0
+def dimmer_cycle():
 
-    while pulse_tick < pulse:
-
-        led.on()
-        sleep(cycle * on)
-        led.off()
-        sleep(cycle * off)
-        pulse_tick = pulse_tick + cycle
-
+    led.on()
+    sleep(cycle * on)
     led.off()
-    sleep(pulse)
+    sleep(cycle * off)
+
+
+if pulse == 0:
+
+    while True:
+
+        dimmer_cycle()
+
+else:
+
+    while True:
+
+        pulse_tick = 0
+
+        while pulse_tick < pulse:
+
+            dimmer_cycle()
+            pulse_tick += cycle
+
+        led.off()
+        sleep(pulse)
